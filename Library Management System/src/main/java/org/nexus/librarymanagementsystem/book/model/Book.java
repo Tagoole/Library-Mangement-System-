8d45dfedb2cc48e435e9b3ef7816662484c568c1
Tagoole Mugwa David <tagooledavid4@gmail.com>
2026-09-09 06:47:41 +0300

    Create book model

---
+++ Library Management System/src/main/java/org/nexus/librarymanagementsystem/book/model/Book.java
@@ -0,0 +1,48 @@
+package org.nexus.librarymanagementsystem.book.model;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "books")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String  id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    private String isbn;
+
+    @Column(name = "total_copies", nullable = false)
+    @Min(value = 0)
+    private int totalCopies;
+
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = LocalDateTime.now();
+    }
+}
+
+
