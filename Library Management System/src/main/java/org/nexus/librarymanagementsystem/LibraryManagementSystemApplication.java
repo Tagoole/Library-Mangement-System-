153d76a461913452f8d60c4c24135fefeb81a34a
Tagoole Mugwa David <tagooledavid4@gmail.com>
2026-09-09 10:41:28 +0300

    Add dotenv conf

--- Library Management System/src/main/java/org/nexus/librarymanagementsystem/LibraryManagementSystemApplication.java
+++ Library Management System/src/main/java/org/nexus/librarymanagementsystem/LibraryManagementSystemApplication.java
@@ -1,12 +1,21 @@
 package org.nexus.librarymanagementsystem;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LibraryManagementSystemApplication {
 
+
   public static void main(String[] args) {
+
+    Dotenv dotenv = Dotenv
+            .configure()
+            .ignoreIfMissing()
+            .load();
+
+    dotenv.entries().forEach(entry->System.setProperty(entry.getKey(),entry.getValue()));
     SpringApplication.run(LibraryManagementSystemApplication.class, args);
   }
 }
