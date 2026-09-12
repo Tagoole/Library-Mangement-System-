c6477adbb5a030b823c5d10c7e7aa3a58b0b0241
Tagoole Mugwa David <tagooledavid4@gmail.com>
2026-09-12 11:52:06 +0300

    Finish member api exception handlero

---
+++ Library Management System/src/main/java/org/nexus/librarymanagementsystem/member/exception/ApiExceptionHandler.java
@@ -0,0 +1,63 @@
+package org.nexus.librarymanagementsystem.member.exception;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.nexus.librarymanagementsystem.book.dto.ApiResponse;
+import org.nexus.librarymanagementsystem.member.exception.MemberNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(MemberNotFoundException.class)
+    public ResponseEntity<ApiResponse<Object>> handleNotFound(MemberNotFoundException exception){
+        ApiResponse<Object> body = new ApiResponse<>(
+                "MEMBER NOT FOUND",
+                exception.getMessage(),
+                null
+        );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
+    }
+
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Object>> handleValidation(MethodArgumentNotValidException exception){
+        List<Map<String, String>> errors = exception.getBindingResult()
+                .getFieldErrors().stream()
+                .map(f -> Map.of("field", f.getField(),
+                        "message", f.getDefaultMessage()))
+                .toList();
+
+
+        ApiResponse<Object> body = new ApiResponse<>(
+                "BAD_REQUEST",
+                "Validation Error",
+                Map.of("errors",errors)
+        );
+
+        return ResponseEntity.badRequest().body(body);
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Object>> handleUnExpected(Exception exception){
+        log.error("Unexpected Error",exception );
+        ApiResponse<Object> body = new ApiResponse<>(
+                "INTERNAL SERVER ERROR",
+                "Something Went Wrong",
+                null
+        );
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(body);
+    }
+}
