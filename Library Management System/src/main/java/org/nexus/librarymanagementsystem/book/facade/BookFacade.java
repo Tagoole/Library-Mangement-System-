7fd0453f9bdb58c1d6abfb2291b643ca91a32bd5
Tagoole Mugwa David <tagooledavid4@gmail.com>
2026-09-12 11:09:43 +0300

    Add save method to book facade

---
+++ Library Management System/src/main/java/org/nexus/librarymanagementsystem/book/facade/BookFacade.java
@@ -0,0 +1,27 @@
+package org.nexus.librarymanagementsystem.book.facade;
+
+
+import org.nexus.librarymanagementsystem.book.dto.BookRequest;
+import org.nexus.librarymanagementsystem.book.dto.BookResponse;
+import org.nexus.librarymanagementsystem.book.mapper.BookMapper;
+import org.nexus.librarymanagementsystem.book.model.Book;
+import org.nexus.librarymanagementsystem.book.service.BookService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookFacade {
+    private final BookService bookService;
+    private final BookMapper bookMapper;
+
+    public BookFacade(BookService bookService, BookMapper bookMapper){
+        this.bookService = bookService;
+        this.bookMapper = bookMapper;
+    }
+
+    public BookResponse save(BookRequest bookRequest){
+        Book book = bookMapper.toEntity(bookRequest);
+        Book savedBook = bookMapper.toResponse(book);
+
+        return 
+    }
+}
